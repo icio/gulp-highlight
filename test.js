68678355fb30715ab0483d0a70a01c69ed861451
Ole Ersoy <ole.ersoy@gmail.com>
2015-12-07 14:38:41 -0600

    Updated test

--- test.js
+++ test.js
@@ -3,12 +3,13 @@ var assert = require('assert');
 var gutil = require('gulp-util');
 var highlight = require('./index');
 
-it('should highlight', function (cb) {
+it('should highlight', function(cb) {
   var stream = highlight();
 
-  stream.on('data', function (file) {
+  stream.on('data', function(file) {
+    console.log("FILE CONTENTS: " + file.contents.toString());
     assert.equal(file.relative, 'file.ext');
-    assert.equal(file.contents.toString(), '<code>* <span class="hljs-rules">{ <span class="hljs-rule"><span class="hljs-attribute">box-sizing</span>:<span class="hljs-value"> border-box </span></span></span>}</code>');
+    assert.equal(file.contents.toString(), '<code>* { <span class="hljs-attribute">box-sizing</span>: border-box }</code>');
   });
 
   stream.on('end', cb);
@@ -22,10 +23,10 @@ it('should highlight', function (cb) {
   stream.end();
 });
 
-it('should not highlight', function (cb) {
+it('should not highlight', function(cb) {
   var stream = highlight();
 
-  stream.on('data', function (file) {
+  stream.on('data', function(file) {
     assert.equal(file.relative, 'file.ext');
     assert.equal(file.contents.toString(), '<code class="nohighlight">* { box-sizing: border-box }</code>');
   });
@@ -41,10 +42,10 @@ it('should not highlight', function (cb) {
   stream.end();
 });
 
-it('should highlight html', function (cb) {
+it('should highlight html', function(cb) {
   var stream = highlight();
 
-  stream.on('data', function (file) {
+  stream.on('data', function(file) {
     assert.equal(file.relative, 'file.ext');
     assert.equal(file.contents.toString(), '<code>&lt;<span class="hljs-keyword">div</span>&gt;html&lt;/<span class="hljs-keyword">div</span>&gt;</code>');
   });
@@ -60,10 +61,10 @@ it('should highlight html', function (cb) {
   stream.end();
 });
 
-it('should not HTML encode quotations', function (cb) {
+it('should not HTML encode quotations', function(cb) {
   var stream = highlight();
 
-  stream.on('data', function (file) {
+  stream.on('data', function(file) {
     assert.equal(file.relative, 'file.ext');
     assert.equal(file.contents.toString(), '<code class="json"><span class="hljs-string">"dependencies"</span>: { <span class="hljs-string">"gulp-highlight"</span>: <span class="hljs-string">"^1.0.0"</span> }</code>');
   });
